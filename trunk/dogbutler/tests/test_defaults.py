f9c8c32188e66aae43a424a2ba6bdda25a9b33dd
vsirisanthana@gmail.com <vsirisanthana@gmail.com@2f3fbc26-f680-7eb7-1c31-484541d7f6ba>
2012-04-10 15:26:34 +0000

    Merge DOG-16: Support session

--- trunk/dogbutler/tests/test_defaults.py
+++ trunk/dogbutler/tests/test_defaults.py
@@ -8,7 +8,7 @@ from dogbutler.defaults import (get_default_cache, get_default_cookie_cache, get
 from dogbutler.tests.base import BaseTestCase
 
 
-@patch('requests.get')
+@patch('requests.sessions.Session.request')
 class TestDefaults(BaseTestCase):
 
     def setUp(self):
@@ -23,46 +23,46 @@ class TestDefaults(BaseTestCase):
         set_default_cache(self._orig_default_cache)
         super(TestDefaults, self).tearDown()
 
-    def test_set_default_cache(self, mock_get):
+    def test_set_default_cache(self, mock_request):
         response = Response()
         response.status_code = 200
         response._content = 'Mocked response content'
         response.headers = {
             'Cache-Control': 'max-age=100',
             }
-        mock_get.return_value = response
+        mock_request.return_value = response
 
         C0 = self.cache
         C1 = Cache()
         C2 = Cache()
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 1)
+        self.assertEqual(mock_request.call_count, 1)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 1)
+        self.assertEqual(mock_request.call_count, 1)
 
         set_default_cache(C1)
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 2)
+        self.assertEqual(mock_request.call_count, 2)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 2)
+        self.assertEqual(mock_request.call_count, 2)
 
         set_default_cache(C2)
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
 
         set_default_cache(C0)
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
 
-    def test_disable_default_cache(self, mock_get):
+    def test_disable_default_cache(self, mock_request):
         """
         Test disable default cache (by setting default cache to None)
         """
@@ -72,59 +72,59 @@ class TestDefaults(BaseTestCase):
         response.headers = {
             'Cache-Control': 'max-age=100',
             }
-        mock_get.return_value = response
+        mock_request.return_value = response
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 1)
+        self.assertEqual(mock_request.call_count, 1)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 1)
+        self.assertEqual(mock_request.call_count, 1)
 
         set_default_cache(None)
 
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 2)
+        self.assertEqual(mock_request.call_count, 2)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
         get('http://www.test.com/path')
-        self.assertEqual(mock_get.call_count, 4)
+        self.assertEqual(mock_request.call_count, 4)
 
-    def test_set_default_cookie_cache(self, mock_get):
+    def test_set_default_cookie_cache(self, mock_request):
         response = Response()
         response.headers = {
             'Set-Cookie': 'name=value',
         }
         response.url = 'http://www.test.com/path'
-        mock_get.return_value = response
+        mock_request.return_value = response
 
         C0 = self.cookie_cache
         C1 = Cache()
         C2 = Cache()
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path', cookies={'name': 'value'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', cookies={'name': 'value'}, allow_redirects=True)
 
         set_default_cookie_cache(C1)
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path', cookies={'name': 'value'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', cookies={'name': 'value'}, allow_redirects=True)
 
         set_default_cookie_cache(C2)
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path', cookies={'name': 'value'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', cookies={'name': 'value'}, allow_redirects=True)
 
         set_default_cookie_cache(C0)
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path', cookies={'name': 'value'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', cookies={'name': 'value'}, allow_redirects=True)
 
-    def test_disable_default_cookie_cache(self, mock_get):
+    def test_disable_default_cookie_cache(self, mock_request):
         """
         Test disable default cookie cache (by setting default cookie cache to None)
         """
@@ -133,21 +133,21 @@ class TestDefaults(BaseTestCase):
             'Set-Cookie': 'name=value',
         }
         response.url = 'http://www.test.com/path'
-        mock_get.return_value = response
+        mock_request.return_value = response
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path', cookies={'name': 'value'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', cookies={'name': 'value'}, allow_redirects=True)
 
         set_default_cookie_cache(None)
 
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
         get('http://www.test.com/path')
-        mock_get.assert_called_with('http://www.test.com/path')
+        mock_request.assert_called_with('GET', 'http://www.test.com/path', allow_redirects=True)
 
-    def test_set_default_redirect_cache(self, mock_get):
+    def test_set_default_redirect_cache(self, mock_request):
         response0 = Response()
         response0.url = 'http://www.test.com/neverseemeagain'
         response0.status_code = 301
@@ -164,37 +164,37 @@ class TestDefaults(BaseTestCase):
         }
         response1.history = [response0]
 
-        mock_get.return_value = response1
+        mock_request.return_value = response1
 
         C0 = self.redirect_cache
         C1 = Cache()
         C2 = Cache()
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/redirect_here')
+        mock_request.assert_called_with('GET', 'http://www.test.com/redirect_here', allow_redirects=True)
 
         set_default_redirect_cache(C1)
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/redirect_here')
+        mock_request.assert_called_with('GET', 'http://www.test.com/redirect_here', allow_redirects=True)
 
         set_default_redirect_cache(C2)
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/redirect_here')
+        mock_request.assert_called_with('GET', 'http://www.test.com/redirect_here', allow_redirects=True)
 
         set_default_redirect_cache(C0)
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/redirect_here')
+        mock_request.assert_called_with('GET', 'http://www.test.com/redirect_here', allow_redirects=True)
 
-    def test_disable_default_redirect_cache(self, mock_get):
+    def test_disable_default_redirect_cache(self, mock_request):
         """
         Test disable default redirect cache (by setting default redirect cache to None)
         """
@@ -214,16 +214,16 @@ class TestDefaults(BaseTestCase):
         }
         response1.history = [response0]
 
-        mock_get.return_value = response1
+        mock_request.return_value = response1
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/redirect_here')
+        mock_request.assert_called_with('GET', 'http://www.test.com/redirect_here', allow_redirects=True)
 
         set_default_redirect_cache(None)
 
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
         get('http://www.test.com/neverseemeagain')
-        mock_get.assert_called_with('http://www.test.com/neverseemeagain')
+        mock_request.assert_called_with('GET', 'http://www.test.com/neverseemeagain', allow_redirects=True)
