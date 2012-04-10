913dab0d3cbaf4ceab3f0187e95159f44ef193ee
vsirisanthana <vsirisanthana@gmail.com>
2012-04-10 11:10:56 +0000

    - Move get codes into session

--- dogbutler/tests/test_async.py
+++ dogbutler/tests/test_async.py
@@ -8,12 +8,12 @@ from dogbutler import async
 from dogbutler.tests.base import BaseTestCase
 
 
-@patch('requests.get')
+@patch('requests.sessions.Session.request')
 class TestAsync(BaseTestCase):
 
-    def test_get(self, mock_get):
+    def test_get(self, mock_request):
         # Setup mock
-        def side_effect(url, *args, **kwargs):
+        def side_effect(method, url, *args, **kwargs):
             if '/1' in url:
                 response = Response()
                 response.status_code = 200
@@ -35,7 +35,7 @@ class TestAsync(BaseTestCase):
                 response.status_code = 403
                 response._content = 'Mocked response content Z'
             return response
-        mock_get.side_effect = side_effect
+        mock_request.side_effect = side_effect
 
         requests = [
             ('http://www.test.com/path/1', {}),
@@ -45,7 +45,7 @@ class TestAsync(BaseTestCase):
 
         # Make multiple GET requests
         responses = async.get(requests)
-        self.assertEqual(mock_get.call_count, 3)
+        self.assertEqual(mock_request.call_count, 3)
 
         self.assertEqual(responses[0].status_code, 200)
         self.assertEqual(responses[0].content, 'Mocked response content X')
@@ -59,7 +59,7 @@ class TestAsync(BaseTestCase):
         # Make multiple GET requests again
         # A request to http://www.test.com/path/1 should have been cached
         responses = async.get(requests)
-        self.assertEqual(mock_get.call_count, 4)
+        self.assertEqual(mock_request.call_count, 4)
 
         self.assertEqual(responses[0].status_code, 200)
         self.assertEqual(responses[0].content, 'Mocked response content X')
@@ -75,6 +75,6 @@ class TestAsync(BaseTestCase):
 
         # Make sure the If-None-Match still works
         responses = async.get(requests[:1])
-        self.assertEqual(mock_get.call_count, 5)
+        self.assertEqual(mock_request.call_count, 5)
 
-        mock_get.assert_called_with('http://www.test.com/path/1', headers={'If-None-Match': '"fdcd6016cf6059cbbf418d66a51a6b0a"'})
+        mock_request.assert_called_with('GET', 'http://www.test.com/path/1', headers={'If-None-Match': '"fdcd6016cf6059cbbf418d66a51a6b0a"'}, allow_redirects=True)
